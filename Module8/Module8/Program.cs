a8d0827d691095f4274fa9e946d9e20d28c059f3
Rafael Hernandez <rhernandez7071@southwestuniversity.edu>
2021-06-11 10:09:14 -0600

    Assignment 8

---
+++ Module8/Module8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Module8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demographics<string> Panchita1 = new Demographics<string>();
+            Panchita1.misc = "Rock this town";
+            Panchita1.age = 26;
+
+            Console.WriteLine(Panchita1.misc);
+            Console.WriteLine(Panchita1.age);
+
+        }
+    }
+
+    
+
+    class Demographics<T>
+    {
+        public T misc { get; set; }
+        public int age { get; set; }
+    }
+}
